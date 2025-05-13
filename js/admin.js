2252ecf3dcd94ef74ab4371876f5f2a8bb9323c7
Kamilla Huhnke <kamillahuhnke@me.com>
2025-05-13 12:29:16 +0200

    admin / placeholder

--- js/admin.js
+++ js/admin.js
@@ -108,3 +108,50 @@ if (logoutBtn) {
     window.location.href = 'login.html';
     });
 }
+
+
+
+// === HENT BRUGERDATA ===
+const userId = sessionStorage.getItem('book_app_user_id');
+const token = sessionStorage.getItem('book_app_user_token');
+
+// Redirect hvis brugeren ikke er logget ind
+if (!userId || !token) {
+    window.location.href = 'login.html';
+}
+
+fetch(`http://127.0.0.1:5555/users/${userId}`, {
+    headers: {
+    'X-Session-Token': token
+    }
+})
+
+.then(res => {
+    if (!res.ok) throw new Error('Brugerdata kunne ikke hentes');
+    return res.json();
+})
+
+.then(data => {
+    document.querySelector('#name').textContent = `${data.first_name} ${data.last_name}`;
+    document.querySelector('#email').textContent = data.email;
+    document.querySelector('#birth').textContent = data.birth_date;
+    document.querySelector('#address').textContent = data.address;
+    document.querySelector('#phone').textContent = data.phone_number;
+    document.querySelector('#member').textContent = data.membership_date;
+})
+
+.catch(err => {
+    console.error('Fejl ved indlæsning af brugerdata:', err);
+    alert('Der opstod en fejl under hentning af din profilinformation.');
+});
+
+
+
+document.getElementById('add_books').onclick = () => {
+    document.getElementById('book_form').classList.remove('hidden');
+    document.getElementById('panel').classList.add('hidden');
+};
+document.getElementById('personal_info').onclick = () => {
+    document.getElementById('book_form').classList.add('hidden');
+    document.getElementById('panel').classList.remove('hidden');
+};
--- js/panel.js
+++ js/panel.js
@@ -1,8 +1,11 @@
-document.getElementById('add_books').onclick = () => {
-    document.getElementById('book_form').classList.remove('hidden');
-    document.getElementById('panel').classList.add('hidden');
-};
-document.getElementById('personal_info').onclick = () => {
-    document.getElementById('book_form').classList.add('hidden');
-    document.getElementById('panel').classList.remove('hidden');
-};
+// Bruges ikke pt
+
+
+// document.getElementById('add_books').onclick = () => {
+//     document.getElementById('book_form').classList.remove('hidden');
+//     document.getElementById('panel').classList.add('hidden');
+// };
+// document.getElementById('personal_info').onclick = () => {
+//     document.getElementById('book_form').classList.add('hidden');
+//     document.getElementById('panel').classList.remove('hidden');
+// };
